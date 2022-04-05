b006c736c1b0f8167a53ca6937e5512b19912003
alfaridzimhmmd <Muhammadalfaridzi257@gmail.com>
2022-04-05 16:48:04 +0700

    menambahkan template

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h>Perkenalkan</h1>";
+        echo" Nama saya Muhammad Alfaridzi
+              Saya tinggal di daerah Jakarta Timur
+              Olahraga yang saya sukai adalah
+              Bulutangkis,Futsal";
+
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Muhammad</li>
+ <li>Nama Belakang : Alfaridzi </li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Pisangan Lama 2</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Sepakbola</li>
+ <li>BuluTangkis</li>
+ <li>Basket</li>
+ </ul>
+</section>
