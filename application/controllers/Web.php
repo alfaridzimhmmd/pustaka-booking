c926c6195e57e581912099df2b99b2c2f8f85533
alfaridzimhmmd <Muhammadalfaridzi257@gmail.com>
2022-04-05 17:12:13 +0700

    mengedit index

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -8,6 +8,13 @@ class Web extends CI_Controller
         parent::__construct();
         $this->load->helper('url');
     }
+    public function index()
+    {
+        $data['judul'] = "Halaman depan";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_index', $data);
+        $this->load->view('v_footer', $data);
+    }
     public function about()
     {
         $data['judul'] = "Halaman about";
